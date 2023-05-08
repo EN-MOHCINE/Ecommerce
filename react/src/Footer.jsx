e1a992f2d1b1317807a63f9a5e5f6798c3621206
Mohcine Ennefai <mohcine_ennefai@gmail.com>
2023-05-08 10:38:25 +0100

    update  cards

--- react/src/Footer.jsx
+++ react/src/Footer.jsx
@@ -10,83 +10,83 @@ import send from "./photos/send.svg";
 import BUnique from './photos/BuniqueBig.svg'
 import { Link } from "react-router-dom";
 function Footer() {
-  return (
-    <div className="footer">
-      <div id="logoSec">
-          <img src={BUnique}/>
-      </div>
-      <div id="containerF">
-        <div>
-          <p className="title">contact us</p>
-          <ul>
-            <li className="li"> 
-              <img src={mail} className='iccons lg'/>
-              Bunique@gmail.com
-            </li>
-          </ul>
-          <p className="title follow" >Follow us on</p>
-          <div className="social">
-            <img src={insta} className='iccons'/>
-            <img src={facebook} className='iccons'/>
-            <img src={whatsapp} className='iccons'/>
-            <img src={tiktok} className='iccons'/>
-          </div>
-        </div>
-        <div className="FullScrenDiv">
-          <p className="title">profil</p>
-          <ul>
-          <li className="li">
-                My Account
-            </li> 
-            <li className="li">
-                Login
-            </li> 
-            <li className="li">
-                My cart
-            </li> 
-            <li className="li">
-                Wishlist
-            </li>
-          </ul>
-        </div>
-        <div>
-          <p className="title">services</p>
-          <ul>
-            <li className="li">
-                About us
-            </li>
-            <li className="li">
-                Delivery Information
-            </li>
-          </ul>
-        </div>
-        <div className="FullScrenDiv">
-          <p className="title">Nos cataloges</p>
-          <ul>
-            <li className="li">
-              <Link to='/products/categories/1'>
-                Djellaba
-              </Link>
-            </li>
-            <li className="li">
-            <Link to='/products/categories/3'>Kaftan</Link>
-            </li>
-            <li className="li">
-                <Link to='/products/categories/5'>Abaya</Link>
-            </li>
-            <li className="li">
-                <Link to='/products/categories/4'>Takchita</Link>
-            </li>
-            <li className="li">
-                <Link to='/products/categories/2'>Gandora</Link>
-            </li> <li className="li">
-                <Link to='/products/categories/6'>Dress</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+  // return (
+  //   <div className="footer">
+  //     <div id="logoSec">
+  //         <img src={BUnique}/>
+  //     </div>
+  //     <div id="containerF">
+  //       <div>
+  //         <p className="title">contact us</p>
+  //         <ul>
+  //           <li className="li"> 
+  //             <img src={mail} className='iccons lg'/>
+  //             Bunique@gmail.com
+  //           </li>
+  //         </ul>
+  //         <p className="title follow" >Follow us on</p>
+  //         <div className="social">
+  //           <img src={insta} className='iccons'/>
+  //           <img src={facebook} className='iccons'/>
+  //           <img src={whatsapp} className='iccons'/>
+  //           <img src={tiktok} className='iccons'/>
+  //         </div>
+  //       </div>
+  //       <div className="FullScrenDiv">
+  //         <p className="title">profil</p>
+  //         <ul>
+  //         <li className="li">
+  //               My Account
+  //           </li> 
+  //           <li className="li">
+  //               Login
+  //           </li> 
+  //           <li className="li">
+  //               My cart
+  //           </li> 
+  //           <li className="li">
+  //               Wishlist
+  //           </li>
+  //         </ul>
+  //       </div>
+  //       <div>
+  //         <p className="title">services</p>
+  //         <ul>
+  //           <li className="li">
+  //               About us
+  //           </li>
+  //           <li className="li">
+  //               Delivery Information
+  //           </li>
+  //         </ul>
+  //       </div>
+  //       <div className="FullScrenDiv">
+  //         <p className="title">Nos cataloges</p>
+  //         <ul>
+  //           <li className="li">
+  //             <Link to='/products/categories/1'>
+  //               Djellaba
+  //             </Link>
+  //           </li>
+  //           <li className="li">
+  //           <Link to='/products/categories/3'>Kaftan</Link>
+  //           </li>
+  //           <li className="li">
+  //               <Link to='/products/categories/5'>Abaya</Link>
+  //           </li>
+  //           <li className="li">
+  //               <Link to='/products/categories/4'>Takchita</Link>
+  //           </li>
+  //           <li className="li">
+  //               <Link to='/products/categories/2'>Gandora</Link>
+  //           </li> <li className="li">
+  //               <Link to='/products/categories/6'>Dress</Link>
+  //           </li>
+  //         </ul>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default Footer;
